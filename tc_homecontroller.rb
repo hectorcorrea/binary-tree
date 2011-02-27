06751942e6ce495b7ce4ca4c73fad011af4b0f82
Hector Correa <hector@hectorcorrea.com>
2011-02-27 15:48:35 -0500

    fixed broken unit test for home controller

--- tc_homecontroller.rb
+++ tc_homecontroller.rb
@@ -4,7 +4,7 @@ require "homecontroller"
 class TestHomeController < Test::Unit::TestCase
   def test_return_html
     controller = HomeController.new(nil)
-    html = controller.process_request()
+    html = controller.get_html_view()
     assert_not_nil(html)
   end
 end
